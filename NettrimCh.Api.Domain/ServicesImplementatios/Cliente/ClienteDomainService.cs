832c850de731d5c8cae99420fada0acf847db50f
miguelpz@gmail.com <miguelpz@gmail.com>
2020-03-31 20:42:21 +0200

    Fix Errors

--- NettrimCh.Api.Domain/ServicesImplementatios/Cliente/ClienteDomainService.cs
+++ NettrimCh.Api.Domain/ServicesImplementatios/Cliente/ClienteDomainService.cs
@@ -3,6 +3,7 @@ using NettrimCh.Api.Domain.Entities;
 using NettrimCh.Api.Domain.Mapping.Extension.Cliente;
 using NettrimCh.Api.Domain.ServicesContracts.Cliente;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NettrimCh.Api.Domain.Services.Cliente
 {
@@ -19,12 +20,21 @@ namespace NettrimCh.Api.Domain.Services.Cliente
 
         public IEnumerable<ClienteEntity> GetAll()
         {
-            var clientList = _clienteRepository.GetAll().Result;
-            return clientList.toEntity();                        
+            try
+            {
+                var clientList = _clienteRepository.GetAll().Result;
+                return clientList.toEntity();
+            }
+            catch
+            {
+                return Enumerable.Empty<ClienteEntity>();
+            }
         }
 
         public bool Update(int id, ClienteEntity clienteEntity)
         {
+            if (clienteEntity.Id != id) return false;
+            
             try
             {
                 return _clienteRepository.Update(id, clienteEntity.toModel()).Result;
