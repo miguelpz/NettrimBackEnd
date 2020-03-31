11a6e7663009ace08c50c3ab564352a13b957d50
Sara Porcar Palacios <sararagon.5@live.com>
2020-03-31 12:41:44 +0200

    Introduciendo Proyecto...

---
+++ NettrimCh.Api.Application.Contracts/ServiceContracts/Proyecto/IProyectoApplicationService.cs
@@ -0,0 +1,12 @@
+﻿using NettrimCh.Api.Application.Contracts.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NettrimCh.Api.Application.Contracts.ServiceContracts.Proyecto
+{
+    public interface IProyectoApplicationService
+    {
+        IEnumerable<ProyectoDto> GetAll();
+    }
+}
